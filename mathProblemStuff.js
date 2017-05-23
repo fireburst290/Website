c69dbb70a9d164e45f53f8be0ed814d9d0d34602
fireburst290 <littlesir06@gmail.com>
2017-05-23 10:09:05 -0400

    Update mathProblemStuff.js

--- mathProblemStuff.js
+++ mathProblemStuff.js
@@ -2,6 +2,7 @@ var spin1;
 var spin2;
 var operater1;
 
-var spin1 = (Math.floor(Math.random() * 10) + 1;);
+var spin1 = getElementyid("demo").inerHTML =
+    (Math.floor(Math.random() * 10) + 1;);
 console.log(spin1);
 
